8c7ed1a1f0ddb5b7e8769ed5eef1102e59fd6b30
ger16 <ger16or@gmail.com>
2015-11-17 09:46:25 +0100

    les vrais choses

---
+++ src/test/java/TestTricherche.java
@@ -0,0 +1,74 @@
+
+public class TestTricherche {
+
+	private int[] tab;
+	private int x;
+	
+	public TestTricherche(){
+		this.tab = new int[10];
+		this.x = 1;
+	}
+	public TestTricherche(int[] tab, int x){
+		this.tab = tab;
+		this.x = x;
+	}
+	public void setVal (int y){
+		this.x=y;
+	}
+	public int getVal(){
+		return this.x;
+	}
+	public int []getTab(){
+		return this.tab;
+	}
+	public void setTab(int []t){
+		this.tab=t;
+	}
+	
+	public boolean verifTri(int []tab){
+		for(int i=0 ; i<this.tab.length; i++){
+			if(tab[i]>tab[i+1])
+				return false;
+			else
+				return true;
+		}
+		return false;
+	}
+
+	public boolean verifEntier(){
+		for (int i=0 ; i<this.tab.length ; i++){
+			if (this.tab[i]>0)
+				return true;
+		}
+		return false;
+	}
+	public boolean verifValNull(int []tab){
+		for(int i=0 ; i<this.tab.length ;i++){
+			if(tab[i]==0)
+				return true;
+		}
+		return false;
+	}
+	
+	public boolean elementExist(int []tab,int x)
+	{
+		for(int i=0 ; i<this.tab.length ; i++){
+			if(tab[i]==x)
+				return true;
+		}
+		return false;
+	}
+	
+	public boolean verifTailleTab(int []tab)
+	{
+		int count =0;
+		for(int i=0 ; i<this.tab.length ; i++){
+				count++;
+		}
+		
+		if(count!=this.tab.length)
+			return false;
+		else 
+			return true;
+	}
+}
